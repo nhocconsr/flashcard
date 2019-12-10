13d3cffde35c67b3e6641b081094a9372f5fc1e3
noi22 <yy.inoi@gmail.com>
2019-12-10 18:37:36 +0900

    modify flashcard functionality by transformer_page_view

--- lib/BeginnerQestions.dart
+++ lib/BeginnerQestions.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flip_card/flip_card.dart';
-
-
+import 'package:transformer_page_view/transformer_page_view.dart';
+import 'utils/buildin_transformers.dart';
 
 
 class Questions extends StatefulWidget {
@@ -16,103 +16,89 @@ class Questions extends StatefulWidget {
 }
 
 class QuestionsState extends State<Questions> {
-  final String url = "http://54.250.202.29/questions";
+
   List data;
-  int _index = 0;
+  List questionsData;
 
-  Future<String> getSWData() async {
-    var res =
-    await http.get(Uri.parse(url), headers: {"Accept": "application/json"});
-
+  Future getData() async {
+    http.Response response = await http.get("http://54.250.202.29/questions");
+    data = json.decode(response.body);
     setState(() {
-      var resBody = json.decode(res.body);
-      data = resBody;
-    });
-
-    return "Success!";
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.name),
-        backgroundColor: Colors.lightGreen,
-      ),
-      body: Container(
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                     FlipCard(
-                      direction: FlipDirection.HORIZONTAL, // default
-                      front: Card(
-                        child: Container(
-                          padding: EdgeInsets.all(15.0),
-                          child: Row(
-                            children: <Widget>[
-                              Text("Question: "),
-                              Text(data[_index]["question"],
-                                  style: TextStyle(
-                                      fontSize: 18.0, color: Colors.black87)),
-                            ],
-                          )),
-                      ),
-                      back: Card(
-                        child: Container(
-                          padding: EdgeInsets.all(15.0),
-                          child: Row(
-                            children: <Widget>[
-                              Text("Answer: "),
-                              Text(data[_index]["answer"],
-                                  style: TextStyle(
-                                      fontSize: 18.0, color: Colors.black87)),
-                            ],
-                          )
-                        ),
-                      ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        floatingActionButtonLocation:
-          FloatingActionButtonLocation.centerDocked,
-            floatingActionButton: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  FloatingActionButton(
-                    onPressed: _decrementIndex,
-                    child: Icon(Icons.navigate_before),
-              ),
-                  FloatingActionButton(
-                    onPressed: _incrementIndex,
-                    child: Icon(Icons.navigate_next),
-              )
-            ],
-          ),
-        )
-         );
-  }
-
-  void _decrementIndex() {
-    setState(() {
-      _index--;
-    });
-   }
-
-  void _incrementIndex() {
-    setState(() {
-      _index++;
+      questionsData = data;
     });
   }
 
   @override
   void initState() {
     super.initState();
-    this.getSWData();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (questionsData != null) {
+
+      List<Color> list = [
+        Colors.redAccent,
+        Colors.blueAccent,
+        Colors.greenAccent
+      ];
+
+      return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.name),
+            backgroundColor: Colors.lightGreen,
+          ),
+          body:
+          TransformerPageView(
+              loop: true,
+              transformer: new ZoomOutPageTransformer(),
+              itemBuilder: (BuildContext context, int index) {
+                return new Container(
+                  child: new Center(
+                    child: FlipCard(
+                      direction: FlipDirection.HORIZONTAL,
+                      front: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Card(
+                          color: list[index % list.length],
+                          child:Center(
+                            child: Text(
+                              "${questionsData[index]["question"]}",
+                              textAlign: TextAlign.center,
+                              style: new TextStyle(
+                                  fontSize: 80.0, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                      back: Container(
+                          padding: EdgeInsets.all(15.0),
+                          child: Card(
+                            color: Colors.lime,
+                            child: Center(
+                              child: Text(
+                                "${questionsData[index]["answer"]}",
+                                textAlign: TextAlign.center,
+                                style: new TextStyle(
+                                    fontSize: 80.0, color: Colors.black),
+                              ),
+                            ),
+                          )
+                      ),
+                    ),
+                  ),
+                );
+              },
+              itemCount: questionsData.length)
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.name),
+          backgroundColor: Colors.lightGreen,
+        ),
+      );
+    }
   }
 }
