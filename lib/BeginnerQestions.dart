637c51ec217bd4fe640340f6b0064cf70d2db53d
noi22 <yy.inoi@gmail.com>
2019-12-12 18:42:49 +0900

    re first commit

---
+++ lib/BeginnerQestions.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flip_card/flip_card.dart';
+import 'package:transformer_page_view/transformer_page_view.dart';
+import 'utils/buildin_transformers.dart';
+
+
+class Questions extends StatefulWidget {
+  final String name;
+  Questions(this.name);
+
+  @override
+  QuestionsState createState() => QuestionsState();
+}
+
+class QuestionsState extends State<Questions> {
+
+  List data;
+  List questionsData;
+
+  Future getData() async {
+    http.Response response = await http.get("http://54.250.202.29/questions");
+    data = json.decode(response.body);
+    setState(() {
+      questionsData = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (questionsData != null) {
+
+      List<Color> list = [
+        Colors.redAccent,
+        Colors.blueAccent,
+        Colors.greenAccent
+      ];
+
+      return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.name),
+            backgroundColor: Colors.teal,
+          ),
+          body:
+          TransformerPageView(
+              loop: true,
+              transformer: new ZoomOutPageTransformer(),
+              itemBuilder: (BuildContext context, int index) {
+                return new Container(
+                  child: new Center(
+                    child: FlipCard(
+                      direction: FlipDirection.HORIZONTAL,
+                      front: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Card(
+                          color: list[index % list.length],
+                          child:Center(
+                            child: Text(
+                              "${questionsData[index]["question"]}",
+                              textAlign: TextAlign.center,
+                              style: new TextStyle(
+                                  fontSize: 80.0, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                      back: Container(
+                          padding: EdgeInsets.all(15.0),
+                          child: Card(
+                            color: Colors.lime,
+                            child: Center(
+                              child: Text(
+                                "${questionsData[index]["answer"]}",
+                                textAlign: TextAlign.center,
+                                style: new TextStyle(
+                                    fontSize: 80.0, color: Colors.black),
+                              ),
+                            ),
+                          )
+                      ),
+                    ),
+                  ),
+                );
+              },
+              itemCount: questionsData.length)
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.name),
+          backgroundColor: Colors.teal,
+        ),
+      );
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import './category1.dart';
+import './login.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//    home: new Login(),
+//   ));
+// }
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    Login.tag: (context) => Login(),
+    Category1.tag: (context) => Category1(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'duelingo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        fontFamily: 'Nunito',
+      ),
+      home: Login(),
+      routes: routes,
+    );
+  }
+}
+  
