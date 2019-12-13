a7d550870055057a5871d6a2f19ecdffa0819625
NaoArimura <naoyosetani@gmail.com>
2019-12-13 16:23:45 +0900

    added condidion for checking user exist or not in  wrapper.dart

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,13 +1,25 @@
 import 'package:duelingo_flashcard/authenticate/authenticate.dart';
-import 'package:duelingo_flashcard/home/home.dart';
+import 'package:duelingo_flashcard/category1.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './models/user.dart';
+import './category1.dart';
 
 
 class Wrapper extends StatelessWidget{
   @override
   Widget build(BuildContext context) { 
 
+
+   final user = Provider.of<User>(context);
+   print(user);
+
+
     //return either Home or Category1 widget
-    return Authenticate();
+    if(user == null){
+      return Authenticate();
+    } else {
+      return Category1();
+    }
   }
 }
